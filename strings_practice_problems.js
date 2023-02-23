525b46e733d4086e1d66ae9890cd6da1cf21d9e4
Makayla <makaylabusher@gmail.com>
2023-02-23 14:44:23 -0600

    string practice 1-3 plus install prompt-sync

---
+++ strings_practice_problems.js
@@ -0,0 +1,38 @@
+// # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+var firstName = "Mark";
+var lastName = "Smith";
+
+console.log(firstName + " " + lastName);
+
+// # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+var firstName = "Lee";
+var lastName= "Green";
+
+console.log(`${firstName} ${lastName}`);
+
+// # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+const prompt = require("prompt-sync")({sigint:true}); 
+var word = prompt("Please enter a word:");
+
+if (word === "marco") {
+  console.log("polo");
+}
+
+// # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+
+// # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+// # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+// # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+// # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
